dbeef9d5ab59324306bba06aa876922a8d2f560e
Alexandra Cune <alexandra.cune@yahoo.com>
2023-05-25 08:21:12 +0300

    task 2

--- app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/practicaltest02/PracticalTest02MainActivity.java
@@ -3,9 +3,20 @@ package ro.pub.cs.systems.eim.practicaltest02;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class PracticalTest02MainActivity extends AppCompatActivity {
 
+    private EditText serverPortEditText = null;
+
+    // Client widgets
+    private EditText clientAddressEditText = null;
+    private EditText clientPortEditText = null;
+    private EditText wordEditText = null;
+    private TextView dictionaryTextView = null;
+
+//    private ServerThread serverThread = null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
